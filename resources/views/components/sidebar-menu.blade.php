316f69dd928af8326104ec31f2a879c9fd1a939b
Thanos Talridis <a.talaridis@darkpony.com>
2021-04-28 00:06:38 +0300

    style: change names of sidebar

--- resources/views/components/sidebar-menu.blade.php
+++ resources/views/components/sidebar-menu.blade.php
@@ -2,27 +2,27 @@
     <ul class="text-gray-600 space-y-5 dark:text-white font-semibold  ">
         <li>
             <a href="{{route('users.index')}}"
-               class="dark:hover:text-gray-400 dark:text-white hover:text-gray-900 text-gray-500 transition ease-in duration-200">User </a>
+               class="dark:hover:text-gray-400 dark:text-white hover:text-gray-900 text-gray-500 transition ease-in duration-200">Χρηστες</a>
         </li>
         <li>
             <a href="{{route('storage.index')}}"
-               class="dark:hover:text-gray-400 dark:text-white hover:text-gray-900 text-gray-500 transition ease-in duration-200">Storage</a>
+               class="dark:hover:text-gray-400 dark:text-white hover:text-gray-900 text-gray-500 transition ease-in duration-200">Αποθήκη</a>
         </li>
         <li>
             <a href="{{route('storage.add')}}"
-               class="dark:hover:text-gray-400 dark:text-white hover:text-gray-900 text-gray-500 transition ease-in duration-200">Add Storage</a>
+               class="dark:hover:text-gray-400 dark:text-white hover:text-gray-900 text-gray-500 transition ease-in duration-200">Καταχώρηση ΔΑ προς Αθήνα</a>
         </li>
         <li>
             <a href="{{route('storage.remove')}}"
-               class="dark:hover:text-gray-400 dark:text-white hover:text-gray-900 text-gray-500 transition ease-in duration-200">Remove Storage</a>
+               class="dark:hover:text-gray-400 dark:text-white hover:text-gray-900 text-gray-500 transition ease-in duration-200">Καταχώρηση πωλήσεων </a>
         </li>
         <li>
             <a href="{{route('information.index')}}"
-               class="dark:hover:text-gray-400 dark:text-white hover:text-gray-900 text-gray-500 transition ease-in duration-200">Information</a>
+               class="dark:hover:text-gray-400 dark:text-white hover:text-gray-900 text-gray-500 transition ease-in duration-200">Αρχειο ΔΑ προς Αθηνα</a>
         </li>
         <li>
             <a href="{{route('sales.index')}}"
-               class="dark:hover:text-gray-400 dark:text-white hover:text-gray-900 text-gray-500 transition ease-in duration-200">Sales</a>
+               class="dark:hover:text-gray-400 dark:text-white hover:text-gray-900 text-gray-500 transition ease-in duration-200">Αρχειο πωλήσεων</a>
         </li>
     </ul>
 
